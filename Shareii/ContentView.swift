73b2577d60d9bc7aaf2db77e672c81ae031694e9
Bilal Rami Sukkar <bilal_sukkar@icloud.com>
2020-09-15 18:34:04 +0200

    Initial Commit

---
+++ Shareii/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shareii
+//
+//  Created by Bilal Rami Sukkar on 15.09.20.
+//  Copyright © 2020 Bilal Rami Sukkar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
